1de4f3ecf8520e0c729058152025ee2623fbeb68
JOHNDRAJ <95675004+JOHNDRAJ@users.noreply.github.com>
2025-03-13 02:41:48 -0700

    changes schema

--- SQL Files/create_tables.sql
+++ SQL Files/create_tables.sql
@@ -4,19 +4,13 @@ CREATE TABLE Customers (
     address VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Categories (
-	catID INT PRIMARY KEY AUTO_INCREMENT,
-    catname VARCHAR(100) UNIQUE NOT NULL
-);
-
 CREATE TABLE Items (
 	iID INT PRIMARY KEY AUTO_INCREMENT,
     iname VARCHAR(100) NOT NULL,
     price DECIMAL(10, 2) NOT NULL,
     description VARCHAR(200) NOT NULL,
-    catID INT NOT NULL,
-    stock INT NOT NULL,
-    FOREIGN KEY (catID) REFERENCES Categories(catID)
+    catname ENUM('Men', 'Women', 'House Appliance', 'Plants', 'Electronics'),
+    stock INT NOT NULL
 );
 
 CREATE TABLE Carted (
@@ -26,17 +20,26 @@ CREATE TABLE Carted (
     PRIMARY KEY (cID, iID),
     FOREIGN KEY (cID) REFERENCES Customers(cID),
     FOREIGN KEY (iID) REFERENCES Items(iID)
-);
+);	
 
 CREATE TABLE Purchases (
 	pID INT PRIMARY KEY AUTO_INCREMENT,
 	cID INT NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    price double NOT NULL,
     FOREIGN KEY (cID) REFERENCES Customers(cID)
+);
+
+CREATE TABLE Reviews (
+	rID INT PRIMARY KEY AUTO_INCREMENT,
+	iID INT NOT NULL,
+    review varchar(200) NOT NULL,
+    FOREIGN KEY (iID) REFERENCES Items(iID)
 );
 
 -- drop table Carted;
 -- drop table Purchases;
 -- drop table Items;
+-- drop table Reviews
 -- drop table Categories;
 -- drop table Customers;
